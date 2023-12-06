2523c539e146897ef1ba2c705b25f4a330f1df10
Ayoub KEBAILI <keayoub@msn.com>
2023-12-06 15:15:22 -0500

    update script

--- DeployApp.ps1
+++ DeployApp.ps1
@@ -4,10 +4,11 @@ cd..
 zip -j backend.zip ./backend/*
 cd frontend
 Remove-Item * -Recurse; Copy-Item -Path "../../../apps/frontend/*" -Destination "./" -Recurse -Force; Copy-Item -Path "../../../apps/frontend/pages/*" -Destination "./pages"; Copy-Item -Path "../../../common/*" -Destination "./" -Recurse -force;pip install -r requirements.txt
-cd ..
-cd frontend
-zip -r  ../frontend.zip ./frontend
+cd ../frontend
+zip -r ../frontend.zip ./*
+# Return to target folder
 cd ..
 #az webapp deployment source config-zip --resource-group "rg-oaixoalab04-uq5k6hqnso4yc" --name "webApp-Backend-BotId-sjfiuqfabeg52" --src "backend.zip"
 #az webapp deployment source config-zip --resource-group "rg-oaixoalab04-uq5k6hqnso4yc" --name "webApp-Frontend-sjfiuqfabeg52" --src "frontend.zip"
+# Return to root folder
 cd ../..
